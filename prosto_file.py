55be0650f5b06ce781786e7cc73cc6c410026f15
Vasua Pupkin <you@gmail.com>
2023-05-01 22:14:53 +0300

    new

--- prosto_file.py
+++ prosto_file.py
@@ -7,7 +7,6 @@ prosto_spisok22 = [2, 4, 5, 8, 2, 9, 22, 33, 43]
 prosto_spisok23 = [2, 4, 5, 8, 2, 9, 22, 33, 43]
 
 
-for i in prosto_spisok22:
-    for j in prosto_spisok23:
-        result2 = prosto_func(num=i, step=j)
-        print(result2)
+for i, j in zip(prosto_spisok22, prosto_spisok23):
+    result2 = prosto_func(num=i, step=j)
+    print(result2)
